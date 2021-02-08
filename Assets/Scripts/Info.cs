ad95833b3e15d459c3207d98b7e168c23a14fe2b
Albalo <alonzo_lona_soraoto@hotmail.com>
2021-02-08 01:54:18 -0600

    Recorrido Virtual Cuicuilco

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Info : MonoBehaviour
+{
+    public GameObject information;
+    public Collider player;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        //SceneManager.LoadScene(1);
+        information.SetActive(true);
+    }
+
+}
